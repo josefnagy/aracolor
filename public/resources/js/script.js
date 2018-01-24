266ef6461e6fdd95888d3938eee9709ca7bc3a0e
Josef Nagy <pe4ce@MBpro.local>
2018-01-24 15:15:02 +0100

    opt for mobile

--- public/resources/js/script.js
+++ public/resources/js/script.js
@@ -64,9 +64,9 @@ if( arrowRight !== null && arrowLeft !== null ) {
 
 $(document).ready(function() {
 
-  $(".scrolldown-anim__link").click(function(event){
-    event.preventDefault();
-  });
+  // $(".scrolldown-anim__link").click(function(event){
+  //   event.preventDefault();
+  // });
 
   var offset = 250;
   var duration = 300;
